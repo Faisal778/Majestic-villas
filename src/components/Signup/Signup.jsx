cf86b4fab6ca141789017dfeef930615751f5eac
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-17 02:14:22 +0600

    password must be of minimum 6 charachter including one of both upper and lower case alphabet

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -32,8 +32,7 @@ const Signup = () => {
         showConfirmButton: false,
         timer: 1500,
       });
-      
-      
+
       console.log("Profile updated successfully");
       console.log(data.name, data.photoURL);
       navigate("/");
@@ -135,9 +134,27 @@ const Signup = () => {
                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
                 type="password"
                 name="password"
-                {...register("password", { required: true })}
+                {...register("password", {
+                  required: true,
+                  minLength: 6,
+                  pattern: /(?=.*[A-Z])(?=..*[a-z])/,
+                })}
               />
-              {errors.password && <span className="text-red-400">password is required*</span>}
+              {errors.password?.type === "required" && (
+                <p role="alert" className="text-red-600">
+                  Password is required
+                </p>
+              )}
+              {errors.password?.type === "minLength" && (
+                <p role="alert" className="text-red-600">
+                  Password must have minimum length of 6
+                </p>
+              )}
+              {errors.password?.type === "pattern" && (
+                <p role="alert" className="text-red-600">
+                  Password must consist one upper case and one lower case alphabet.
+                </p>
+              )}
             </div>
 
             <div className="mt-6">
