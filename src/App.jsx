9effbd8517f5c33d1753d0e0b70c92003b6d5db3
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-16 11:01:56 +0600

    initial react+vite, tailwindCSS and daisyUI setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <span className="loading loading-dots loading-xs"></span>
+      <span className="loading loading-dots loading-sm"></span>
+      <span className="loading loading-dots loading-md"></span>
+      <span className="loading loading-dots loading-lg"></span>
+    </>
+  );
+}
+
+export default App;
