d483afe45e9f7f3392030e13eb267d26e2c2c386
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-16 13:16:02 +0600

    navbar added

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3cegYCMYooNORIAl-VKxvuOkdKV0EJgA",
+  authDomain: "majestic-villas.firebaseapp.com",
+  projectId: "majestic-villas",
+  storageBucket: "majestic-villas.appspot.com",
+  messagingSenderId: "848513921960",
+  appId: "1:848513921960:web:5b144f92400140fef9a39d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Root/Root.jsx";
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
   },
 ]);
 
