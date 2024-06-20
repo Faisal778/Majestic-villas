cba02ef080e939aeca837a41969bb7c10b1ef9ba
Faisal778 <62201274+Faisal778@users.noreply.github.com>
2024-06-20 21:20:48 +0600

    react-slick slider added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Estates from './Estates';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
         <Estates></Estates>
         </div>
     );
